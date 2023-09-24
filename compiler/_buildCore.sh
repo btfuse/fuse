4a98c5cc6de18b432127e240fa25f4fb02edf8c8
Norman Breau <norman@nbsolutions.ca>
2023-09-24 19:38:25 -0300

    add deprecations to existing compiler scripts

--- compiler/_buildCore.sh
+++ compiler/_buildCore.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo "_buildCore.sh script is deprecated"
+
 # Build Core Lib
 cd fuse-js
 npx tsc
--- compiler/_buildTestProjects.sh
+++ compiler/_buildTestProjects.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo "_buildTestProjects.sh script is deprecated"
+
 # Build the test echo plugin
 cd plugins/echo
 npm install file:../../fuse-js/fuse.tgz
