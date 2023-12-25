2e8a9a3d9b5d19bc90299f075219514968e11b63
Norman Breau <norman@breautek.com>
2023-12-25 09:46:06 -0400

    fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,18 +6,18 @@ if [ -z "$fork" ]; then
 fi
 
 echo "Checking Fuse Docs..."
-if [ -z "$(readlink -f ./fuse-docs)" ]; then
+if [ ! -z "$(readlink -f ./fuse-docs)" ]; then
     git clone git@github.com:$fork/fuse-docs.git --recurse-submodules
 fi
 
 echo "Checking Fuse JS..."
-if [ -z "$(readlink -f ./fuse-js)" ]; then
+if [ ! -z "$(readlink -f ./fuse-js)" ]; then
     git clone git@github.com:$fork/fuse-js.git --recurse-submodules
 fi
 
 if [ `uname` == "Darwin" ]; then
     echo "Checking Fuse iOS..."
-    if [ -z "$(readlink -f ./fuse-ios)" ]; then
+    if [ ! -z "$(readlink -f ./fuse-ios)" ]; then
         git clone git@github.com:$fork/fuse-ios.git --recurse-submodules
     fi
 else
@@ -25,16 +25,16 @@ else
 fi
 
 echo "Checking Fuse Android..."
-if [ -z "$(readlink -f ./fuse-android)" ]; then
+if [ ! -z "$(readlink -f ./fuse-android)" ]; then
     git clone git@github.com:$fork/fuse-android.git --recurse-submodules
 fi
 
 echo "Checking Fuse Test App..."
-if [ -z "$(readlink -f ./fuse-test-app)" ]; then
+if [ ! -z "$(readlink -f ./fuse-test-app)" ]; then
     git clone git@github.com:$fork/fuse-test-app.git --recurse-submodules
 fi
 
 echo "Checking Fuse Echo Plugin..."
-if [ -z "$(readlink -f ./fuse-echo)" ]; then
+if [ ! -z "$(readlink -f ./fuse-echo)" ]; then
     git clone git@github.com:$fork/fuse-echo.git --recurse-submodules
 fi
