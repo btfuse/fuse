b056e5b90bbf93748a19741fc46150993eefbded
Norman Breau <norman@breautek.com>
2024-10-06 18:10:09 -0300

    removed the MacOS required because we no longer use DocC

--- docs/build.sh
+++ docs/build.sh
@@ -16,8 +16,6 @@
 source ../build-tools/DirectoryTools.sh
 source ../build-tools/assertions.sh
 
-assertMac "Mac is required to build Fuse Documentation"
-
 echo "Building Fuse JS Reference Documentation..."
 npx typedoc --options typedoc.fusejs.json
 
@@ -28,23 +26,6 @@ spopd
 
 echo "Building Fuse iOS Reference Documentation..."
 doxygen ../ios/Doxyfile
-# spushd ../ios
-#     xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuse -configuration Release -destination 'generic/platform=iOS' docbuild
-#     xcodebuild -quiet -workspace BTFuse.xcworkspace -scheme BTFuseTestTools -configuration Release -destination 'generic/platform=iOS' docbuild
-
-#     iosBuild=$(echo "$(xcodebuild -workspace BTFuse.xcworkspace -scheme BTFuse -configuration Release -sdk iphoneos -showBuildSettings | grep -E '^\s*CONFIGURATION_BUILD_DIR =' | awk -F '= ' '{print $2}' | xargs)")
-#     simBuild=$(echo "$(xcodebuild -workspace BTFuse.xcworkspace -scheme BTFuse -configuration Debug -sdk iphonesimulator -showBuildSettings | grep -E '^\s*CONFIGURATION_BUILD_DIR =' | awk -F '= ' '{print $2}' | xargs)")
-
-#     rm -rf ../docs/build
-#     mkdir -p ../docs/build
-    
-#     rm -rf ../docs/docs/ref/fuse-ios
-#     mkdir -p ../docs/docs/ref/fuse-ios
-
-#     xcrun docc convert ./Overview.docc  --fallback-display-name BTFuse --output-dir ../docs/build/Overview.doccarchive
-#     xcrun docc merge ../docs/build/Overview.doccarchive $iosBuild/BTFuse.doccarchive $iosBuild/BTFuseTestTools.doccarchive --output-path ../docs/build/merged.doccarchive
-#     xcrun docc process-archive transform-for-static-hosting ../docs/build/merged.doccarchive --hosting-base-path ref/fuse-ios --output-path ../docs/docs/ref/fuse-ios/
-# spopd
 
 rm -rf ./docs/ref/fuse-android
 cp -r ../android/fuse/build/docs/javadoc ./docs/ref/fuse-android
