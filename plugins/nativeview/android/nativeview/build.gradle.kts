ff70dcfd7c58bafd562d01a48ca5ea39512bd4f9
Norman Breau <norman@breautek.com>
2025-04-10 22:25:18 -0300

    fix: native view android repo url

--- plugins/nativeview/android/nativeview/build.gradle.kts
+++ plugins/nativeview/android/nativeview/build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
 
     repositories {
         maven {
-            url = uri("https://nexus.breautek.com/repository/breautek")
+            url = uri("https://nexus.breautek.com/repository/android")
             credentials {
                 username = findProperty("breautek.repository.user").toString()
                 password = findProperty("breautek.repository.password").toString()
